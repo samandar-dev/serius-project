0d9987bd4317c834844233f23686ee722922edc5
samandar-dev <samandarbohodirov01@gmail.com>
2022-09-26 22:26:22 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import HomePage from './companents/HomePage';
 import { Route, Routes } from 'react-router-dom';
 import NotFound from './companents/NotFound/NotFound';
 import './App.scss';
+import Navbar from './companents/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <section className='serius'>
         <div className="app">
+          <Navbar />
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/home' element={<HomePage />} />
